746680b9b48c5f94211577658553f0779fd1e5c0
BingbinLiu <289778998@qq.com>
2021-04-09 11:54:13 +0200

    5

---
+++ 5April/welcome.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+    <?php
+    $name = $email = "";
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+       $name = test_input($_POST["name"]);
+       $email = test_input($_POST["email"]);
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data, ENT_QUOTES);
+        return $data;
+    }
+    ?>
+    <p>欢迎<?php echo $name; ?>,您的表单已提交成功！</p>
+    <p>更多信息会发送到您的邮箱：<?php echo $email;?></p>
+</body>
+</html>
